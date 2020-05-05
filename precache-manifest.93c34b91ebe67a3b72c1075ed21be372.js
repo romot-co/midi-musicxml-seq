b62731426bcfcae5b12e0685dcbe2b51bf442fce
romot-co <roboticsurinse@gmail.com>
2020-05-05 09:54:47 +0900

    Updates

---
+++ precache-manifest.93c34b91ebe67a3b72c1075ed21be372.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45d431303cac7060532302a7583068f",
+    "url": "/midi-musicxml-seq/index.html"
+  },
+  {
+    "revision": "a31b19fcf153b4879299",
+    "url": "/midi-musicxml-seq/static/css/main.6160d4c6.chunk.css"
+  },
+  {
+    "revision": "c8e29d9e062f55abe6fe",
+    "url": "/midi-musicxml-seq/static/js/2.55de4c4b.chunk.js"
+  },
+  {
+    "revision": "994a830c7fc6db59d16909b3a36327c7",
+    "url": "/midi-musicxml-seq/static/js/2.55de4c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab13f8582d9f2e3ecb40",
+    "url": "/midi-musicxml-seq/static/js/3.d1fd2432.chunk.js"
+  },
+  {
+    "revision": "f0fd80f5a3205c64299a",
+    "url": "/midi-musicxml-seq/static/js/4.499dfc35.chunk.js"
+  },
+  {
+    "revision": "a31b19fcf153b4879299",
+    "url": "/midi-musicxml-seq/static/js/main.8161b9dc.chunk.js"
+  },
+  {
+    "revision": "00a4f81eaf4ebb2a2bc9",
+    "url": "/midi-musicxml-seq/static/js/runtime-main.a9bc197f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/midi-musicxml-seq/precache-manifest.d05758dab52aea08b1e0faa094b6bc90.js"
+  "/midi-musicxml-seq/precache-manifest.93c34b91ebe67a3b72c1075ed21be372.js"
 );
 
 self.addEventListener('message', (event) => {
